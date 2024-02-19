3af654fdbf09226cbe8fe97120cb6291a206e83c
Jot43 <139986366+Jot43@users.noreply.github.com>
2024-02-19 16:55:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,9 +29,9 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/79cb702524b9
 CODE = (environ.get('CODE', 'https://graph.org/file/ed0504d221445c3c6d47a.jpg'))
 
 #stream link shortner
-STREAM_SITE = (environ.get('STREAM_SITE', 'ziplinker.net'))
-STREAM_API = (environ.get('STREAM_API', 'ae0832f39e24094a0661626a792e6a2b8880e310'))
-STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/'))
+STREAM_SITE = (environ.get('STREAM_SITE', 'shareus.io'))
+STREAM_API = (environ.get('STREAM_API', 'PPrfEdbYHDaV3GFqRvvtGy712nj1'))
+STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/kmdbkup/156'))
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
@@ -66,7 +66,7 @@ PORT = environ.get("PORT", "8080")
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/kmdoffical')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/kmdupdt')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/kmdbkup/156')
-IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
+IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : Jot')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '')) #Log channel id ( make sure bot is admin )
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/kmdsupport_svc') #Support group link ( make sure bot is admin )
